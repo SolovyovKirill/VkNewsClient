8a21cfd1b4578e635554dd9f257c0f9723ca916a
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2023-03-31 16:45:51 +0300

    added comments screen

--- app/src/main/java/com/k_salauyou/vknewsclient/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/k_salauyou/vknewsclient/ui/theme/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.k_salauyou.vknewsclient.MainViewModel
+import com.k_salauyou.vknewsclient.domain.PostComment
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
@@ -24,46 +25,58 @@ fun HomeScreen(
 ) {
     val feedPosts = viewModel.feedPosts.observeAsState(listOf())
 
-    LazyColumn(
-        modifier = Modifier.padding(paddingValues),
-        contentPadding = PaddingValues(
-            top = 16.dp,
-            start = 8.dp,
-            end = 8.dp,
-            bottom = 72.dp
-        ),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(
-            items = feedPosts.value,
-            key = { it.id }
-        ) { feedPost ->
-            val dismissState = rememberDismissState()
-            if (dismissState.isDismissed(DismissDirection.EndToStart)) {
-                viewModel.remove(feedPost)
-            }
-            SwipeToDismiss(
-                modifier = Modifier.animateItemPlacement(),
-                state = dismissState,
-                background = {},
-                directions = setOf(DismissDirection.EndToStart)
-            ) {
-                PostCard(
-                    feedPost = feedPost,
-                    onViewsClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onCommentClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onLikeClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onShareClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
+    if (feedPosts.value.isNotEmpty()) {
+        val comments = mutableListOf<PostComment>().apply {
+            repeat(20) {
+                add(
+                    PostComment(id = it)
                 )
             }
         }
+        CommentsScreen(feedPost = feedPosts.value[0], comments = comments)
     }
+
+
+//    LazyColumn(
+//        modifier = Modifier.padding(paddingValues),
+//        contentPadding = PaddingValues(
+//            top = 16.dp,
+//            start = 8.dp,
+//            end = 8.dp,
+//            bottom = 72.dp
+//        ),
+//        verticalArrangement = Arrangement.spacedBy(8.dp)
+//    ) {
+//        items(
+//            items = feedPosts.value,
+//            key = { it.id }
+//        ) { feedPost ->
+//            val dismissState = rememberDismissState()
+//            if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+//                viewModel.remove(feedPost)
+//            }
+//            SwipeToDismiss(
+//                modifier = Modifier.animateItemPlacement(),
+//                state = dismissState,
+//                background = {},
+//                directions = setOf(DismissDirection.EndToStart)
+//            ) {
+//                PostCard(
+//                    feedPost = feedPost,
+//                    onViewsClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                    onCommentClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                    onLikeClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                    onShareClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                )
+//            }
+//        }
+//    }
 }
