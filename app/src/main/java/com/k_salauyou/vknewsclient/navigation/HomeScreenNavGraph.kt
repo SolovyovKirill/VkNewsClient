dba50da2a0a4136c1198e3307ab8f9bc6b04bcf7
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2023-04-03 12:25:44 +0300

    passing parameters (Jetpack Navigation) part 2
    navigation types

--- app/src/main/java/com/k_salauyou/vknewsclient/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/k_salauyou/vknewsclient/navigation/HomeScreenNavGraph.kt
@@ -2,9 +2,12 @@ package com.k_salauyou.vknewsclient.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.k_salauyou.vknewsclient.domain.FeedPost
+import com.k_salauyou.vknewsclient.navigation.Screen.Companion.KEY_CONTENT_TEXT
 import com.k_salauyou.vknewsclient.navigation.Screen.Companion.KEY_FEED_POST_ID
 
 fun NavGraphBuilder.homeScreenNavGraph(
@@ -18,9 +21,25 @@ fun NavGraphBuilder.homeScreenNavGraph(
         composable(Screen.NewsFeed.route) {
             newsFeedScreenContent()
         }
-        composable(Screen.Comments.route) {
+        composable(
+            route = Screen.Comments.route,
+            arguments = listOf(
+                navArgument(KEY_FEED_POST_ID) {
+                    type = NavType.IntType
+                },
+                navArgument(KEY_CONTENT_TEXT) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
             val feedPostId = it.arguments?.getInt(KEY_FEED_POST_ID) ?: 0
-            commentsScreenContent(FeedPost(id = feedPostId))
+            val contentText = it.arguments?.getString(KEY_CONTENT_TEXT) ?: ""
+            commentsScreenContent(
+                FeedPost(
+                    id = feedPostId,
+                    contentText = contentText
+                )
+            )
         }
     }
 }
